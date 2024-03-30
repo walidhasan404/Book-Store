a56741705b8238dc8db7ed52bec926c5c54c2f6a
mdolidehasan <olidehasan444@gmail.com>
2024-03-30 07:20:23 +0600

    book details commit

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
             <p className='mb-4'><span className='text-lg font-medium'>Email Address:</span> contact@example.com</p>
             <p className='mb-4'><span className='text-lg font-medium'>Phone Number:</span> 123-456-7890</p>
             <p className='mb-4'><span className='text-lg font-medium'>Physical Address:</span> 123 Fake Street, Fictionville, FS 12345</p>
-            <p className='mb-4'><span className='text-lg font-medium'>Social Media Links:</span>
+            <p><span className='text-lg font-medium'>Social Media Links:</span>
                 Facebook: https://www.facebook.com/examplebookstore <br />
                 Twitter: https://twitter.com/examplebookstore <br />
                 Instagram: https://www.instagram.com/examplebookstore</p>
